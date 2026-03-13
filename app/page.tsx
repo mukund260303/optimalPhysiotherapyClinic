fd25d23a111d45ccdc817e0a46467624274dfbf2
Balmukund Patidar <mukunpatidar@gmail.com>
2026-03-14 00:28:14 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -99,7 +99,7 @@ className="bg-blue-600 text-white px-10 py-6 rounded-[2rem] font-black text-lg f
     <div className="bg-white p-6 rounded-[3rem] shadow-xl text-center max-w-[260px]">
       
       <img 
-        src="/doctors/dr-image.png"
+        src="https://github.com/mukund260303/clinik_optimal/blob/main/public/gallery/dr-Ravina.png"
         alt="Dr Pavan Patidar"
         className="w-32 h-32 md:w-40 md:h-40 rounded-full object-cover border-4 border-blue-600 mx-auto"
       />
